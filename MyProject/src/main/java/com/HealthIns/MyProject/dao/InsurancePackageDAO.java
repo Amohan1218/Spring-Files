971f2d9fbd1be4f7fd376c1f9be9e2a14331f39c
Mohan <137370382+Amohan1218@users.noreply.github.com>
2023-09-26 15:36:06 +0530

    Add files via upload

---
+++ MyProject/src/main/java/com/HealthIns/MyProject/dao/InsurancePackageDAO.java
@@ -0,0 +1,11 @@
+package com.HealthIns.MyProject.dao;
+
+import java.util.List;
+
+import com.HealthIns.MyProject.model.InsurancePackage;
+
+public interface InsurancePackageDAO {
+
+	List<InsurancePackage> getAllPackages();
+
+}
---
+++ MyProject/src/main/java/com/HealthIns/MyProject/repository/InsurancePackageRepository.java
@@ -0,0 +1,11 @@
+package com.HealthIns.MyProject.repository;
+
+import java.util.List;
+
+import com.HealthIns.MyProject.model.InsurancePackage;
+
+public interface InsurancePackageRepository {
+
+	List<InsurancePackage> getAllPackagesList();
+
+}
